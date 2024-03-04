ee31f47ed3c2ff792f1fb02530317ab2e8908928
linda-20 <lindaaron40@gmail.com>
2024-03-04 07:18:27 +0300

    create a for loop which iterates through each number in the list

--- challenge2.py
+++ challenge2.py
@@ -1,9 +1,23 @@
 def sum_of_digits(num):
     return sum(int(digit) for digit in str(num))
 
+
 # Define the solution function to find the maximum sum of two numbers
 def solution(A):
     digit_sums = {}
     max_sum = -1
+    
+ # Iterate through each number in the list
+    for num in A:
+        digit_sum = sum_of_digits(num)
+        if digit_sum in digit_sums:
+            max_sum = max(max_sum, num + digit_sums[digit_sum])
+        else:
+            digit_sums[digit_sum] = num
 
- 
+    return max_sum
+
+
+print(solution([51, 71, 17, 42]))  
+print(solution([42, 33, 60]))     
+print(solution([51, 32, 43]))      
