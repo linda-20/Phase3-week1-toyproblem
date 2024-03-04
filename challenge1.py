5647e192a7ba8947a1a85343572bf59d8125a66c
linda-20 <lindaaron40@gmail.com>
2024-03-04 11:41:33 +0300

    define a fuction that calculates the total number of bricks and count of boxes

---
+++ challenge1.py
@@ -0,0 +1,5 @@
+def solution(A):
+    total_bricks = sum(A)
+    box_count = len(A)
+    
+   
--- challenge2.py
+++ challenge2.py
@@ -7,17 +7,18 @@ def solution(A):
     digit_sums = {}
     total_sum = -1
     
- # Iterate through each number in the list
+ # Iterates through each number in the list
     for num in A:
         digit_sum = sum_of_digits(num)
         if digit_sum in digit_sums:
             total_sum = max(total_sum, num + digit_sums[digit_sum])
         else:
             digit_sums[digit_sum] = num
+ 
+# return total sum
+    return total_sum   
 
-    return total_sum
 
-
-print(solution([51, 71, 17, 42]))  
+print(solution([51, 71, 17, 42])) 
 print(solution([42, 33, 60]))     
 print(solution([51, 32, 43]))      
